2a15040f61e6ba53b6ed6c98b842f0800989fd77
katerinasolntsye <105854911+katerinasolntsye@users.noreply.github.com>
2022-12-12 22:15:45 +0300

    Add files via upload

---
+++ go-password-auth-example1/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+const hashCost = 8
+
+var db *sql.DB
+
+func main() {
+	// "Signin" and "Signup" are handler that we will implement
+	http.HandleFunc("/signin", Signin)
+	http.HandleFunc("/signup", Signup)
+	// initialize our database connection
+	initDB()
+	// start the server on port 8000
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func initDB() {
+	var err error
+	// Connect to the postgres db
+	//you might have to change the connection string to add your database credentials
+	db, err = sql.Open("postgres", "dbname=testsignup host=localhost port=5432 user=root password=12312 sslmode=disable")
+	// dbname=mydb sslmode=disable
+	// dbname=testsignup sslmode=disable
+	// "postgres://root:12312@localhost:5432/test_db"
+	// user=root password=12345 dbname=name sslmode=disable
+	// postgres://root:12312@localhost:5432/testsignup
+	fmt.Println(db)
+	if err != nil {
+		log.Print(err)
+		panic(err)
+	}
+}
